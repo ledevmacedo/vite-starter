c4568398de6ecc21b1663b85cd49dbecee009799
Leandro Macedo <leandro.smacedo@Live.com>
2025-06-19 23:13:10 +0100

    feat: add darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from './components/mode-toggle'
 
 function App() {
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
+      <div className="flex min-h-svh space-y-10 flex-col items-center justify-center">
+        <h1 className='text-6xl font-bold'>VITE STARTER</h1>
+        <p className='text-xl text-muted-foreground'>
+          This is a simple vite project containing tailwind.css and Shadcn/Ui.
+        </p>
+        <div className='flex  items-center gap-2'>
+          <Button>Click me</Button>
+          <ModeToggle />
+        </div>
       </div>
     </>
   )
